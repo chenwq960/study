85ae3abc99efda096b0727226b673f1aab8b078f
宋印赠 <songyinzeng@gmail.com>
2019-09-08 13:15:16 +0800

    提交User类定义

---
+++ core-java/src/com/day01/User.java
@@ -0,0 +1,25 @@
+package com.day01;
+
+/**
+ * 
+ * 用户类
+ * 
+ * @author songyz<br>
+ * @createTime 2019-09-08 13:07:43
+ */
+public class User {
+
+    /**
+     * 说方言
+     * 
+     * @param lang 方言名称
+     * @param content 说话内容
+     */
+    public static void say(String lang, String content) {
+        System.out.println("使用方言：" + lang + "，内容：" + content);
+
+        String username, namdf1, namdf2, namdf3, namdf4, namdf5, namdf6, namdf7, namdf8, namdf9, namdf0, namdf11,
+                namdf12, namdf13, namdf14;
+    }
+
+}
